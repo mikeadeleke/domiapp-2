c2df0a5f516ffcc4b41b671bc5bc18f140023219
Mike Adeleke <madeleke13@gmail.com>
2013-11-12 23:37:45 -0600

    Need to fix problem in Gallery controller

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,5 +1,5 @@
 class Gallery < ActiveRecord::Base
 	belongs_to :listing
-
+	mount_uploader :image, ImageUploader
 	validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131113050043) do
+ActiveRecord::Schema.define(version: 20131113053008) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20131113050043) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   create_table "listings", force: true do |t|
